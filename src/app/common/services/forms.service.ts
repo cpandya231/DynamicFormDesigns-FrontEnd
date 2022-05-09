cbe9a1ada90a13eec5750c9f98233c86dea83f3c
Akash Patel <aspatel2312@gmail.com>
2022-05-09 12:31:42 +0530

    review comment fixed

--- src/app/common/services/forms.service.ts
+++ src/app/common/services/forms.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IGetFormTemplateResponse } from 'src/app/forms/form.model';
-
+import { ServiceUtil } from '../../services/utility/ServiceUtil';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,11 +12,11 @@ export class FormsService {
   constructor(private http: HttpClient) { }
 
   GetFormTemplates() {
-    return this.http.get('http://localhost:8080/forms/');
+    return this.http.get(`${ServiceUtil.API_ENDPOINT}/forms`);
   }
 
   GetFormTemplate(formName: any): Observable<IGetFormTemplateResponse> {
-    return this.http.get<IGetFormTemplateResponse>(`http://localhost:8080/forms/${formName}/`);
+    return this.http.get<IGetFormTemplateResponse>(`${ServiceUtil.API_ENDPOINT}/forms/${formName}/`);
   }
 
   SaveFormTemplate(formTemplate: any, name: string, id: number) {
@@ -27,7 +27,7 @@ export class FormsService {
     if (id) {
       body = {...body, ...{id}}
     }
-    return this.http.post('http://localhost:8080/forms/save', body,{
+    return this.http.post(`${ServiceUtil.API_ENDPOINT}/forms/save`, body,{
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
     })
