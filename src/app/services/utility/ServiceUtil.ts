8c8cc5d306ec272ecffe9925a9b48645bcbdf7df
System Administrator <theprime23@gmail.com>
2023-08-05 11:02:44 +0530

    Update backend url

--- src/app/services/utility/ServiceUtil.ts
+++ src/app/services/utility/ServiceUtil.ts
@@ -1,11 +1,11 @@
-import { HttpHeaders } from "@angular/common/http";
+import { HttpHeaders } from '@angular/common/http';
 
 export class ServiceUtil {
-    public static ENV = 'ec2-13-127-223-146.ap-south-1.compute.amazonaws.com';
-    public static API_ENDPOINT = `http://${ServiceUtil.ENV}:8080`;
-    public static HTTPOPTIONS = {
-        headers: new HttpHeaders({
-            'Content-Type': 'application/x-www-form-urlencoded',
-        })
-    };
+  public static ENV = 'ec2-13-233-144-148.ap-south-1.compute.amazonaws.com';
+  public static API_ENDPOINT = `http://${ServiceUtil.ENV}:8080`;
+  public static HTTPOPTIONS = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded',
+    }),
+  };
 }
